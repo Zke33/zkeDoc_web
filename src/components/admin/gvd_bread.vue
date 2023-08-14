a15ff2d1f47274c08c9f278ce44b64bd0ce0d5e2
fengfeng <fengfengzhidao@126.com>
2023-08-15 01:51:10 +0800

    面包屑

--- src/components/admin/gvd_bread.vue
+++ src/components/admin/gvd_bread.vue
@@ -1,14 +1,36 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <a-breadcrumb>
-    <a-breadcrumb-item>首页</a-breadcrumb-item>
-    <a-breadcrumb-item>个人中心</a-breadcrumb-item>
-    <a-breadcrumb-item>用户信息</a-breadcrumb-item>
+    <template  v-for="item in breadList">
+      <a-breadcrumb-item v-if="item.title !== ''">{{ item.title }}</a-breadcrumb-item>
+    </template>
   </a-breadcrumb>
 </template>
+
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+import {ref, watch} from "vue";
+import type {Ref} from "vue";
+
+const route = useRoute()
+const breadList: Ref<breadType[]> = ref([])
+
+interface breadType {
+  title: string
+  name?: string
+}
+
+watch(() => route.matched, (value) => {
+  breadList.value = []
+  value.forEach(item => {
+    breadList.value.push({
+      title: item.meta.title as string,
+      name: item.name as string,
+    })
+  })
+}, {immediate: true})
+
+
+</script>
 
 <style scoped>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -17,10 +17,16 @@ const router = createRouter({
             path: '/admin',
             name: 'admin',
             component: () => import("@/views/admin/admin.vue"),
+            meta: {
+                title: "首页",
+            },
             children: [
                 {
                     path: '',
                     name: 'admin_home',
+                    meta: {
+                        title: "",
+                    },
                     children: [
                         {
                             path: "",
@@ -32,32 +38,49 @@ const router = createRouter({
                 {
                     path: 'user_center',
                     name: 'user_center',
+                    meta: {
+                        title: "个人中心",
+                    },
                     children: [
                         {
                             path: 'info',
                             name: 'info',
+                            meta: {
+                                title: "个人信息",
+                            },
                             component: () => import("@/views/admin/user_center/user_info.vue")
                         },
                         {
                             path: 'coll',
                             name: 'coll',
+                            meta: {
+                                title: "收藏列表",
+                            },
                             component: () => import("@/views/admin/user_center/user_coll_docs.vue")
                         },
                     ]
                 },
-
                 {
                     path: 'auths',
                     name: 'auths',
+                    meta: {
+                        title: "权限管理",
+                    },
                     children: [
                         {
                             path: 'users',
                             name: 'users',
+                            meta: {
+                                title: "用户列表",
+                            },
                             component: () => import("@/views/admin/role_mgr/user_list.vue")
                         },
                         {
                             path: 'roles',
                             name: 'roles',
+                            meta: {
+                                title: "角色列表",
+                            },
                             component: () => import("@/views/admin/role_mgr/role_list.vue")
                         },
                     ]
@@ -65,25 +88,36 @@ const router = createRouter({
                 {
                     path: 'settings',
                     name: 'settings',
+                    meta: {
+                        title: "系统管理",
+                    },
                     children: [
                         {
                             path: 'logs',
                             name: 'logs',
+                            meta: {
+                                title: "日志列表",
+                            },
                             component: () => import("@/views/admin/settings_mgr/logs_list.vue")
                         },
                         {
                             path: 'images',
                             name: 'images',
+                            meta: {
+                                title: "图片列表",
+                            },
                             component: () => import("@/views/admin/settings_mgr/images_list.vue")
                         },
                         {
                             path: 'sites',
                             name: 'sites',
+                            meta: {
+                                title: "站点配置",
+                            },
                             component: () => import("@/views/admin/settings_mgr/sites.vue")
                         }
                     ]
                 },
-
             ]
         }
     ]
