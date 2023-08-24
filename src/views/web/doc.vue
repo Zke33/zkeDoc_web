be37dff6c8806ee0122d1fddb5abab01c97d0626
fengfeng <fengfengzhidao@126.com>
2023-08-24 20:14:25 +0800

    文档目录展示

--- src/views/web/doc.vue
+++ src/views/web/doc.vue
@@ -16,28 +16,38 @@
           </div>
         </div>
         <div class="doc_body">
-          <MdPreview :model-value="text" :theme="store.theme"></MdPreview>
+          <MdPreview :editorId="id" :model-value="text" :theme="store.theme"></MdPreview>
         </div>
       </main>
       <section>
-        文档目录
+        <div class="head">
+          文档目录
+        </div>
+        <div class="body">
+          <MdCatalog :editorId="id" :scrollElement="scrollElement"/>
+        </div>
+
       </section>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {MdPreview} from 'md-editor-v3';
+import {MdPreview, MdCatalog} from 'md-editor-v3';
 import {ref} from "vue";
 import Gvd_slide from "@/components/web/gvd_slide.vue";
 import Gvd_fixed_menu from "@/components/web/gvd_fixed_menu.vue";
 import LongMd from "@/assets/md/long.md?raw"
 import SortMd from "@/assets/md/sort.md?raw"
+import catalogMd from "@/assets/md/catalog.md?raw"
 import 'md-editor-v3/lib/style.css';
 import {useStore} from "@/stores";
 
+const id = 'preview-only';
+const scrollElement = document.documentElement;
 const store = useStore()
-const text = ref(LongMd)
+const text = ref(catalogMd)
+
 </script>
 
 <style lang="scss">
@@ -68,21 +78,25 @@ const text = ref(LongMd)
     > main {
       width: calc(100% - 240px);
       padding: 40px 20px 0 40px;
-      .doc_head{
+
+      .doc_head {
         color: var(--color-text-2);
-        h1{
+
+        h1 {
           color: var(--md-theme-color);
         }
-        .doc_data{
+
+        .doc_data {
           margin-bottom: 10px;
-          >span{
+
+          > span {
             margin-right: 10px;
           }
         }
       }
 
-      .md-editor-preview-wrapper{
-       padding: 0;
+      .md-editor-preview-wrapper {
+        padding: 0;
       }
     }
 
@@ -96,6 +110,61 @@ const text = ref(LongMd)
       right: 0;
       top: 0;
       z-index: 1;
+      color: var(--color-text-2);
+
+      .head {
+        height: 40px;
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid var(--doc_border);
+        padding: 0 20px;
+      }
+
+      .body {
+        padding: 10px 20px;
+        max-height: calc(100vh - 400px);
+        overflow-y: auto;
+
+        &:hover{
+          &::-webkit-scrollbar{
+            background-color: #eee;
+          }
+          &::-webkit-scrollbar-thumb {
+            background-color: #999;
+            box-shadow: inset 1px 1px 2px rgba(0, 0, 0, .1);
+            border: 1px solid #d8d8d8;
+          }
+        }
+
+        &::-webkit-scrollbar {
+          background-color: transparent;
+          transition: all 0.3s;
+          width: 8px;
+        }
+
+
+        &::-webkit-scrollbar-thumb {
+          background-color: transparent;
+          border-radius: 4px;
+          box-shadow: inset 1px 1px 2px transparent;
+          border: 1px solid transparent;
+        }
+      }
+
+      .md-editor-catalog-link > span {
+
+        &:hover {
+          color: rgb(var(--arcoblue-6));
+        }
+      }
+
+      .md-editor-catalog-active > span {
+        color: rgb(var(--arcoblue-5));
+
+        &:hover {
+          color: rgb(var(--arcoblue-6));
+        }
+      }
     }
   }
 }
